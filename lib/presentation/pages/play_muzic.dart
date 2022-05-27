402fa3c14583a7b50842a176756dbca751fad08b
vahidmohammadisan <vahidmohammadisan@gmail.com>
2022-05-27 18:44:53 +0430

    changing audio player was implemented

--- lib/presentation/pages/play_muzic.dart
+++ lib/presentation/pages/play_muzic.dart
@@ -1,8 +1,14 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:audioplayer/audioplayer.dart';
 import 'package:azeri/domain/entities/Muzic.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-
-import '../widgets/audio_player_widget.dart';
+import 'package:http/http.dart';
+import 'package:path_provider/path_provider.dart';
 
 class PlayMuzic extends StatelessWidget {
   PlayMuzic({this.muzic, this.onPressed});
@@ -17,10 +23,276 @@ class PlayMuzic extends StatelessWidget {
         backgroundColor: Colors.amber,
         title: Text(muzic.Name.toString()),
       ),
-      body: AudioPlayerWidget(
-        url: muzic.Link,
-        isAsset: false,
+      body: AudioApp(muzic.Link),
+    );
+  }
+}
+
+enum PlayerState { stopped, playing, paused }
+
+typedef void OnError(Exception exception);
+
+class AudioApp extends StatefulWidget {
+  AudioApp(this.url);
+
+  String url;
+
+  @override
+  _AudioAppState createState() => _AudioAppState(url);
+}
+
+class _AudioAppState extends State<AudioApp> {
+  _AudioAppState(this.url);
+
+  String url;
+
+  Duration duration;
+  Duration position;
+
+  AudioPlayer audioPlayer;
+
+  String localFilePath;
+
+  PlayerState playerState = PlayerState.stopped;
+
+  get isPlaying => playerState == PlayerState.playing;
+
+  get isPaused => playerState == PlayerState.paused;
+
+  get durationText =>
+      duration != null ? duration.toString().split('.').first : '';
+
+  get positionText =>
+      position != null ? position.toString().split('.').first : '';
+
+  bool isMuted = false;
+
+  StreamSubscription _positionSubscription;
+  StreamSubscription _audioPlayerStateSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    initAudioPlayer();
+  }
+
+  @override
+  void dispose() {
+    _positionSubscription.cancel();
+    _audioPlayerStateSubscription.cancel();
+    audioPlayer.stop();
+    super.dispose();
+  }
+
+  void initAudioPlayer() {
+    audioPlayer = AudioPlayer();
+    _positionSubscription = audioPlayer.onAudioPositionChanged
+        .listen((p) => setState(() => position = p));
+    _audioPlayerStateSubscription =
+        audioPlayer.onPlayerStateChanged.listen((s) {
+      if (s == AudioPlayerState.PLAYING) {
+        setState(() => duration = audioPlayer.duration);
+      } else if (s == AudioPlayerState.STOPPED) {
+        onComplete();
+        setState(() {
+          position = duration;
+        });
+      }
+    }, onError: (msg) {
+      setState(() {
+        playerState = PlayerState.stopped;
+        duration = Duration(seconds: 0);
+        position = Duration(seconds: 0);
+      });
+    });
+  }
+
+  Future play() async {
+    await audioPlayer.play(url);
+    setState(() {
+      playerState = PlayerState.playing;
+    });
+  }
+
+  Future _playLocal() async {
+    await audioPlayer.play(localFilePath, isLocal: true);
+    setState(() => playerState = PlayerState.playing);
+  }
+
+  Future pause() async {
+    await audioPlayer.pause();
+    setState(() => playerState = PlayerState.paused);
+  }
+
+  Future stop() async {
+    await audioPlayer.stop();
+    setState(() {
+      playerState = PlayerState.stopped;
+      position = Duration();
+    });
+  }
+
+  Future mute(bool muted) async {
+    await audioPlayer.mute(muted);
+    setState(() {
+      isMuted = muted;
+    });
+  }
+
+  void onComplete() {
+    setState(() => playerState = PlayerState.stopped);
+  }
+
+  Future<Uint8List> _loadFileBytes(String url, {OnError onError}) async {
+    Uint8List bytes;
+    try {
+      bytes = await readBytes(Uri.parse(url));
+    } on ClientException {
+      rethrow;
+    }
+    return bytes;
+  }
+
+  Future _loadFile() async {
+    final bytes = await _loadFileBytes(url,
+        onError: (Exception exception) =>
+            print('_loadFile => exception $exception'));
+
+    final dir = await getApplicationDocumentsDirectory();
+    final file = File('${dir.path}/audio.mp3');
+
+    await file.writeAsBytes(bytes);
+    if (await file.exists()) {
+      setState(() {
+        localFilePath = file.path;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Center(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              '',
+              style: textTheme.headline1,
+            ),
+            Material(child: _buildPlayer()),
+            if (!kIsWeb)
+              localFilePath != null ? Text(localFilePath) : Container(),
+            if (!kIsWeb)
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () => _loadFile(),
+                      child: Text('Download'),
+                    ),
+                    if (localFilePath != null)
+                      ElevatedButton(
+                        onPressed: () => _playLocal(),
+                        child: Text('play local'),
+                      ),
+                  ],
+                ),
+              ),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _buildPlayer() => Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(mainAxisSize: MainAxisSize.min, children: [
+              IconButton(
+                onPressed: isPlaying ? null : () => play(),
+                iconSize: 64.0,
+                icon: Icon(Icons.play_arrow),
+                color: Colors.amber,
+              ),
+              IconButton(
+                onPressed: isPlaying ? () => pause() : null,
+                iconSize: 64.0,
+                icon: Icon(Icons.pause),
+                color: Colors.amber,
+              ),
+              IconButton(
+                onPressed: isPlaying || isPaused ? () => stop() : null,
+                iconSize: 64.0,
+                icon: Icon(Icons.stop),
+                color: Colors.amber,
+              ),
+            ]),
+            if (duration != null)
+              Slider(
+                  value: position?.inMilliseconds?.toDouble() ?? 0.0,
+                  onChanged: (double value) {
+                    return audioPlayer.seek((value / 1000).roundToDouble());
+                  },
+                  min: 0.0,
+                  max: duration.inMilliseconds.toDouble()),
+            if (position != null) _buildMuteButtons(),
+            if (position != null) _buildProgressView()
+          ],
+        ),
+      );
+
+  Row _buildProgressView() => Row(mainAxisSize: MainAxisSize.min, children: [
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: CircularProgressIndicator(
+            value: position != null && position.inMilliseconds > 0
+                ? (position?.inMilliseconds?.toDouble() ?? 0.0) /
+                    (duration?.inMilliseconds?.toDouble() ?? 0.0)
+                : 0.0,
+            valueColor: AlwaysStoppedAnimation(Colors.amber),
+            backgroundColor: Colors.grey.shade400,
+          ),
+        ),
+        Text(
+          position != null
+              ? "${positionText ?? ''} / ${durationText ?? ''}"
+              : duration != null
+                  ? durationText
+                  : '',
+          style: TextStyle(fontSize: 24.0),
+        )
+      ]);
+
+  Row _buildMuteButtons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        if (!isMuted)
+          ElevatedButton.icon(
+            onPressed: () => mute(true),
+            icon: const Icon(
+              Icons.headset_off,
+              color: Colors.amber,
+            ),
+            style: ButtonStyle(
+              backgroundColor: Colors.amber
+            ),
+            label: const Text('Mute', style: TextStyle(color: Colors.white)),
+          ),
+        if (isMuted)
+          ElevatedButton.icon(
+            onPressed: () => mute(false),
+            icon: const Icon(Icons.headset, color: Colors.amber),
+            label: const Text('Unmute', style: TextStyle(color: Colors.white)),
+          ),
+      ],
     );
   }
 }
